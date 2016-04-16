b38532686ad38c8a3822f7b73bf9eaf46fa132a0
Dmitriy <d.mokhnatkin@yandex.ru>
2016-04-16 17:15:15 +0300

    Refactored modification table on item page

--- static/js/store/cart.js
+++ static/js/store/cart.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
     $('.add-to-cart-button').each(function(i, obj) {
     //test
     });
-	$('.add-to-cart-button').bind('change', validateDate);
 });
 
 function addToCart(modificationId) {
